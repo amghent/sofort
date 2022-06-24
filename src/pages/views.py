548c2c9e38638fff907ca6466d1299bffd93e7af
DOSIM000\SIDVINY <yves.vindevogel.external@arcelormittal.com>
2022-06-24 11:45:43 +0200

    Backup

--- src/pages/views.py
+++ src/pages/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 
 
 def detail(request, slug):
-    from core.context import get_default_context
+    from core.context import Context
     from pages.models import Page
 
     page = Page.objects.get(slug=slug)
     
-    context = get_default_context(current_page="pages_detail", slug=slug)
+    context = Context(current_page="pages_detail", slug=slug).get()
     context["load"]["sidebar"] = False
     context["load"]["datatables"] = False
     context["load"]["editor"] = False
