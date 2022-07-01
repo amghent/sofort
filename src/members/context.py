0bbf7cda24a03251521c93382e31f94393e6616d
Yves Vindevogel <yves.vindevogel@asynchrone.com>
2022-07-01 18:21:22 +0200

    Code cleanup

--- src/members/context.py
+++ src/members/context.py
@@ -8,7 +8,6 @@ class Context:
     def get_member_from_username(user: User) -> Member:
         member = None
 
-        print(user)
         if user is not None:
             try:
                 member = Member.objects.get(member_name=user.username.strip().lower())
