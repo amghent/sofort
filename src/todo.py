4fba59b29c588f8d19cd06f2f9711726170c07c1
Yves Vindevogel <vindevoy@users.noreply.github.com>
2022-07-08 14:54:56 +0200

    Features of TinyMCE for a question

--- src/todo.py
+++ src/todo.py
@@ -13,6 +13,16 @@
 # TODO: Edit on a discussion
 
 # TODO: Styling the TinyMCE
-# TODO: Update the features of TinyMCE
 
 # TODO: Get rid of fields in Member that are also in User
+
+# TODO: Init TinyMCE settings in separate files
+
+# TODO: TinyMCE -> Upload files is possible when there's an extra script for accepting the upload
+# TODO: TinyMCE -> Advanced images
+# TODO: TinyMCE -> Import css
+# TODO: TinyMCE -> Insert username via toolbar and menu
+
+# TODO: All the spacing must come from internal settings that can be changed through the interface
+
+# TODO: The list of questions looks so boring
