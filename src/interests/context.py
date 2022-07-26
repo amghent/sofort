83bab301377dda6c55ddb501aeba43cd45467ae4
Yves Vindevogel <vindevoy@users.noreply.github.com>
2022-07-26 10:54:37 +0200

    Removed the parts that are not implemented yet from the sidebar

--- src/interests/context.py
+++ src/interests/context.py
@@ -12,14 +12,14 @@ def get_sidebar(slug: str) -> list:
 
     sb = [
         {"href": reverse(interests_views.interest_group_detail, args=[slug]), "title": "Dashboard"},
-        {"href": reverse(interests_views.interest_group_under_construction, args=[slug]), "title": "Bookmarks"},
+        # {"href": reverse(interests_views.interest_group_under_construction, args=[slug]), "title": "Bookmarks"},
         {"href": reverse(questions_views.index, args=[slug]), "title": "Questions"},
-        {"href": reverse(interests_views.interest_group_under_construction, args=[slug]), "title": "Issues"},
-        {"href": reverse(interests_views.interest_group_under_construction, args=[slug]), "title": "Forums"},
-        {"href": reverse(interests_views.interest_group_under_construction, args=[slug]), "title": "Tutorials"},
-        {"href": reverse(interests_views.interest_group_under_construction, args=[slug]), "title": "Newsletters"},
-        {"href": reverse(interests_views.interest_group_under_construction, args=[slug]), "title": "Tags"},
-        {"href": reverse(interests_views.interest_group_under_construction, args=[slug]), "title": "Links"},
+        # {"href": reverse(interests_views.interest_group_under_construction, args=[slug]), "title": "Issues"},
+        # {"href": reverse(interests_views.interest_group_under_construction, args=[slug]), "title": "Forums"},
+        # {"href": reverse(interests_views.interest_group_under_construction, args=[slug]), "title": "Tutorials"},
+        # {"href": reverse(interests_views.interest_group_under_construction, args=[slug]), "title": "Newsletters"},
+        # {"href": reverse(interests_views.interest_group_under_construction, args=[slug]), "title": "Tags"},
+        # {"href": reverse(interests_views.interest_group_under_construction, args=[slug]), "title": "Links"},
         {"href": reverse(interests_views.interest_group_about, args=[slug]), "title": "About"},
     ]
 
