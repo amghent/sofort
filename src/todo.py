980261c3372cf4f2f9e8e9beeb5924f3ce038053
Yves Vindevogel <yves.vindevogel.external@arcelormittal.com>
2022-08-02 12:02:14 +0200

    Finished factories and code cleanup variable names

--- src/todo.py
+++ src/todo.py
@@ -1,69 +1,45 @@
 # FIXME: todo in chats.jinja2
-
+# FIXME: Spacing below comment is not correct
 # FIXME: Must have some space between toolbar buttons
 # FIXME: Back on detail should have other icon than back on new
-# FIXME: There should be no pure html code in any of the pages called by a view. Move all to the factories
 # FIXME: There a big gap under each question / answer  or reply.  Maybe because of the <p></p> ?
 # FIXME: Responding / commenting should not raise the number of views
 # FIXME: question detail: toolbar makes content shift down
+# FIXME: 404 errors on some of the tinymce plugins
 
 # TODO: disabled tinymce editor should have copy and print button
-# TODO: local vars should start with _ ?
-# TODO: parameters should start with _
-
 # TODO: View: <i class="fa-solid fa-eye"></i>
 # TODO: Message: <i class="fa-solid fa-message"></i>
 # TODO: Comment: <i class="fa-solid fa-comment-dots"></i>
 # TODO: Open / Closed: <i class="fa-solid fa-folder-open"></i>  <i class="fa-solid fa-folder-closed"></i>
 # TODO: Locked: <i class="fa-solid fa-lock"></i>
-
 # TODO: static pages must be updatable via admin with tinymce
-
 # TODO: gamification: add stats on homepage for the user
-
 # TODO: Mail system
 # TODO: Add "add comment" or "answer question" instead of immediately showing the editors
 # TODO: "Search" on datatable is more "Quick filter", so rename the label
 # TODO: Add interest groups in the top menu (1: link, more: dropdown)
-# TODO: make card_factory and use on dashboard and homepage
-# TODO: make message_factory and use in questions
 # TODO: Multi-level menu in questions
-
 # TODO: Delete on a question
 # TODO: Delete on an answer
 # TODO: Delete on a discussion
-
 # TODO: Edit title of a question
 # TODO: Edit on a question
 # TODO: Edit on a response
 # TODO: Edit on a comment
-
 # TODO: Implement "questions not yet seen"
 # TODO: Implement "my question is answered"
-
 # TODO: fontawesome icons on V/R/C (maybe also on created and author)
 # TODO: The fonts can be smaller everywhere
-
 # TODO: Message when question is saved successfully
 # TODO: Message when response is saved successfully
 # TODO: Message when comment is saved successfully
-
 # TODO: Styling the TinyMCE
-
 # TODO: Get rid of fields in Member that are also in User
-
-# TODO: Init TinyMCE settings in separate files
-
 # TODO: TinyMCE -> Upload files is possible when there's an extra script for accepting the upload
 # TODO: TinyMCE -> Advanced images
 # TODO: TinyMCE -> Import css
 # TODO: TinyMCE -> Insert username via toolbar and menu
-
 # TODO: Single Sign On
-# TODO: The list of questions looks so boring
-
 # TODO: Alert on the homepage should be in a message
-
-# FIXME: 404 errors on some of the tinymce plugins
-
 # TODO: API to get the data only
