4e010350b79a043bbc91daf9f06596604cbb38f5
Yves Vindevogel <vindevoy@users.noreply.github.com>
2022-08-02 07:23:10 +0200

    Transfer between home and work

--- src/questions/templates/config/questions/index.js
+++ src/questions/templates/config/questions/index.js
@@ -1,8 +1,5 @@
-/* configuration of the datatable client-side */
-
 $(document).ready(function () {
     $('#questions_dt').DataTable({
         order: [[2, 'desc']],
     });
 });
-// do not change the id of the datatable !
